7a57373131a6c968b05d6b2ba6b303dd3a30c319
Md Fahmid Hasan Sunny <138954795+fahmidsunnny@users.noreply.github.com>
2025-07-31 22:22:59 +0600

    Add files via upload

---
+++ variable_p2.py
@@ -0,0 +1,6 @@
+#Store your first, middle and last name in three different variables and then print your full name using these variables
+first_name='Fahmid'
+middle_name="Hasan"
+last_name="Sunny"
+full_name=first_name+" "+middle_name+" "+last_name
+print(full_name)
