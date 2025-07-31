48a8acf543f783903ed4e64a6a215dd28a9563b6
Md Fahmid Hasan Sunny <138954795+fahmidsunnny@users.noreply.github.com>
2025-07-31 22:13:03 +0600

    Add files via upload

---
+++ prb1.py
@@ -0,0 +1,5 @@
+#Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+birth_year=2004
+current_year=2025
+age=current_year-birth_year
+print(age)
